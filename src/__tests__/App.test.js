2e4e02f30368d0394d3b51b9db4a8066231c9202
Shubh <shubh.scb@gmail.com>
2022-07-29 00:19:35 +0530

    LINTERS ERR FIX

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,10 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import App from "../App";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
 
 describe('Test for App', () => {
-    test('App renders correctly...', () => {
-        const tree = renderer.create(<App />);
-        expect(tree.toJSON()).toMatchSnapshot();
-    })
+  test('App renders correctly...', () => {
+    const tree = renderer.create(<App />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 });
